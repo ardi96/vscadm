a6a76161fb26b88308ef2c35dc099d8be7b9e115
ardi96 <ardi96@gmail.com>
2025-01-15 23:15:16 +0800

    new home page and schedule page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
     return view('home_new');
+});
+
+Route::get('/schedule', function () {
+    return view('schedule');
 });
