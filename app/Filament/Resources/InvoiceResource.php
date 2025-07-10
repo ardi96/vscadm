996b87eb1a16c6eed4fffb074cdbbed9e2fbfdcf
ardi96 <ardi96@gmail.com>
2025-07-10 19:35:57 +0800

    Add 'Nama Org. Tua' column to InvoiceResource table

--- app/Filament/Resources/InvoiceResource.php
+++ app/Filament/Resources/InvoiceResource.php
@@ -48,7 +48,8 @@ class InvoiceResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('invoice_no')->label('No. Invoice')->sortable()->searchable(),
-                TextColumn::make('member.name')->label('Atas Nama')->searchable()->searchable(),
+                TextColumn::make('member.name')->label('Atas Nama')->sortable()->searchable(),
+                TextColumn::make('parent.name')->label('Nama Org. Tua')->sortable()->searchable(),
                 TextColumn::make('invoice_date')->label('Tgl. Invoice')->date('d-M-Y')->searchable()->sortable(),
                 TextColumn::make('description')->label('Keterangan')->searchable()->sortable(),
                 TextColumn::make('item_description')->label('Nama Paket')->searchable()->sortable(),
