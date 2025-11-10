02b78f18c75b4ed73036fa19891fac78a7a3156a
ardi96 <ardi96@gmail.com>
2025-11-10 16:55:15 +0800

    Add logging to ProcessExpiredPayments for job start notification

--- app/Services/ProcessExpiredPayments.php
+++ app/Services/ProcessExpiredPayments.php
@@ -4,11 +4,13 @@ namespace App\Services;
 
 use Midtrans\Config;
 use App\Models\Payment;
+use Illuminate\Support\Facades\Log;
 
 class ProcessExpiredPayments
 {
     public function __invoke()
     {
+        Log::info('Starting ProcessExpiredPayments job');
 
         $expiredPayments = Payment::where('status', 'pending')->where('is_online', true)
             ->get();
