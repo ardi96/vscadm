8f1df056e3ad4eb6a39202b2763dc877019ec17e
ardi96 <ardi96@gmail.com>
2026-01-04 21:46:21 +0800

    Fix indentation in view-payment-attachment component

--- resources/views/infolists/components/view-payment-attachment.blade.php
+++ resources/views/infolists/components/view-payment-attachment.blade.php
@@ -1,5 +1,7 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
-    @if( $getState ) 
-    <iframe src="/storage/{{ $getState() }}" frameborder="0" width="800px" height="400px"></iframe>
+    @if( $getState() ) 
+        <iframe src="/storage/{{ $getState() }}" frameborder="0" width="800px" height="400px"></iframe>
+    @else
+        <p><span>N/A</span></p>
     @endif
 </x-dynamic-component>
